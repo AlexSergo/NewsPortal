f8bc9596954ade39b1acf75a787be85c5c283697
StoneCookie <pravsha63@gmail.com>
2022-04-29 14:49:28 +0300

    layout_complite

--- app/src/main/java/com/example/newsapp/View/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/View/NewsActivity.kt
@@ -2,15 +2,19 @@ package com.example.newsapp.View
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.newsapp.LocalDataSource.Model.Group.GroupEntity
 import com.example.newsapp.LocalDataSource.Model.User.UserEntity
 import com.example.newsapp.databinding.ActivityNewsBinding
 import com.example.newsapp.R
 import com.example.newsapp.SubscribesFragment
 import com.example.newsapp.View.Group.GroupFragment
+import com.example.newsapp.View.MainPage.MainNewsFragment
 import com.example.newsapp.View.Profile.ProfileFragment
+import com.example.newsapp.View.SaveNews.SaveNewsFragment
+import com.example.newsapp.View.Search.SearchFragment
 
-interface NewsActivityCallback{
+interface NewsActivityCallback {
     fun showGroupFragment(group: GroupEntity)
     fun showSubscribesFragment()
 }
@@ -24,18 +28,45 @@ class NewsActivity : AppCompatActivity(), NewsActivityCallback {
         super.onCreate(savedInstanceState)
         val arguments = intent.extras
         val name = arguments?.get("USER_NAME").toString()
-        val email =  arguments?.get("USER_EMAIL").toString()
+        val email = arguments?.get("USER_EMAIL").toString()
         val token = arguments?.get("USER_TOKEN").toString()
         user = UserEntity(name = name, email = email, token = token)
 
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.profile.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.mainPageContainer, ProfileFragment.newInstance(user))
-                .commitNow()
+        binding.navBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainPageContainer, MainNewsFragment.newInstance(user))
+                        .commitNow()
+                }
+                R.id.search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainPageContainer, SearchFragment())
+                        .commitNow()
+                }
+                R.id.subscribe -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainPageContainer, SubscribesFragment.newInstance(user))
+                        .commitNow()
+                }
+                R.id.save_news -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainPageContainer, SaveNewsFragment())
+                        .commitNow()
+                }
+                R.id.profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.mainPageContainer, ProfileFragment.newInstance(user))
+                        .commitNow()
+                }
+            }
+            true
         }
+
+
     }
 
     override fun showGroupFragment(group: GroupEntity) {
